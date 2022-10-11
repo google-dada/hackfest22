c135b361c2aad7e24f8f23f0bb88d89e9ee4a805
google-dada <88309461+google-dada@users.noreply.github.com>
2022-10-11 20:12:56 +0530

    Create menu.java

---
+++ menu.java
@@ -0,0 +1,43 @@
+//complete it
+
+
+package tutorials_jv;
+import java.util.Scanner;
+
+public class menu{
+    public static void main(String[] args){
+
+
+
+    Scanner sc = new Scanner(System.in);
+
+    char ch = 'y';
+
+    while(ch =='y'){
+
+        System.out.println("1: To find the Area of Circle");
+        System.out.println("2: To find the Area of Rectangle");
+        System.out.println("3: To find the Area of Triangle");
+
+        System.out.print("Enter your Choice: ");
+        int n = sc.nextInt();
+
+        switch(n){
+
+//****************************************
+//your code
+
+
+        }
+
+
+       System.out.print("Do you want to continue(y/n): ");
+       ch = sc.next().charAt(0);
+
+
+    }
+
+
+
+    }
+}
